04afe9cd4ec6b6e66d106ead8b2e04a9d691b613
Sona Juhasova <soj@web2media.dk>
2021-12-02 11:08:47 +0100

    css style filled in

---
+++ src/js/components/nav.js
@@ -0,0 +1,18 @@
+export default class NavBar {
+    constructor() {
+      this.template();
+    }
+  
+    template() {
+      document.querySelector("#app").innerHTML += /*html*/ `
+        <nav class="tabbar">
+          <a href="#home">Home</a>
+          <a href="#chat">Chat</a>
+          <a href="#add_listing">Add Listing</a>
+          <a href="#favourites">Favourites</a>
+          <a href="#profile">Profile</a>
+
+        </nav>
+      `;
+    }
+  }
