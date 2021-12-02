59010f168d6079ddf579d1b228a97699085f8011
sandranielsen <eaasmmn@students.eaaa.com>
2021-12-02 10:33:06 +0100

    Nav + parcel fixes

--- js/router.js
+++ js/router.js
@@ -1,7 +1,7 @@
 /* All routes of the SPA */
 const _routes = {
   "#/": "home",
-  "#/create": "create",
+  "#/add": "add",
   "#/fav": "fav",
   "#/profile": "profile",
 };
@@ -9,32 +9,26 @@ const _pages = document.querySelectorAll(".page");
 const _basePath = location.pathname.replace("index.html", ""); // remove index.html from path
 const _navLinks = document.querySelectorAll("nav a");
 
-/**
- * Changing display to none for all pages
- */
+/* Changing display to none for all pages */
 function hideAllPages() {
   for (const page of _pages) {
     page.style.display = "none";
   }
 }
 
-/**
- * Navigating SPA to specific page by given path
- */
+/* Navigating SPA to specific page by given path */
 function navigateTo(path) {
   window.history.pushState({}, path, _basePath + path);
   showPage(path);
 }
 
 function showPage(path) {
-  hideAllPages(); // hide all pages
-  document.querySelector(`#${_routes[path]}`).style.display = "block"; // show page by given path
+  hideAllPages(); // Hide all pages
+  document.querySelector(`#${_routes[path]}`).style.display = "block"; // Show page by given path
   setActiveTab(path);
 }
 
-/**
- * sets active menu item by given path
- */
+/* Sets active menu item by given path */
 function setActiveTab(path) {
   for (const link of _navLinks) {
     if (path === link.getAttribute("href")) {
@@ -45,9 +39,7 @@ function setActiveTab(path) {
   }
 }
 
-/**
- * Attaching event to nav links and preventing default anchor link event
- */
+/* Attaching event to nav links and preventing default anchor link event */
 function attachNavLinkEvents() {
   const navLinks = document.querySelectorAll(".nav-link");
   for (const link of navLinks) {
@@ -59,14 +51,12 @@ function attachNavLinkEvents() {
   }
 }
 
-/**
- * Initialising the router, calling attachNavLinkEvents(), popstate event and navigateTo()
- */
+/* Initialising the router, calling attachNavLinkEvents(), popstate event and navigateTo() */
 function initRouter() {
   attachNavLinkEvents();
   window.addEventListener("popstate", () => showPage(location.hash)); // change page when using back and forth in browser
 
-  let path = "#/"; // default path
+  let path = "#/"; // Default path
   if (_routes[location.hash]) {
     path = location.hash;
   }
